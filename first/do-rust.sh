41272eaeaa520e0fe4689c3d3710749714a5dff1
Theo Belaire <theo.belaire@gmail.com>
2015-11-22 13:52:58 -0500

    Since this is a port of the example 'first', call it as such

--- first/do-rust.sh
+++ first/do-rust.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 mkdir -p out
-LIBS=.rust/lib/gba.json/
+LIBS=~/.multirust/toolchains/nightly/lib/rustlib/gba.json/lib
 mkdir -p $LIBS
 
 # Call rustc to compile libcore for us.
@@ -10,10 +10,9 @@ mkdir -p $LIBS
   rustc --target=gba.json -Z no-landing-pads ../rust/src/libcore/lib.rs \
     --out-dir $LIBS
 
-#rustc -L libs -Z no-landing-pads --target=target.json src/rust-gba.rs -o out/rust-gba.elf
-
 cargo build --target=gba.json --release
 
-arm-none-eabi-objcopy -O binary target/target/release/rust-gba out/rust-gba.gba
-gbafix out/rust-gba.gba
-open out/rust-gba.gba
+# I have no idea what it's in target/gba
+arm-none-eabi-objcopy -O binary target/gba/release/first out/first.gba
+gbafix out/first.gba
+open out/first.gba
