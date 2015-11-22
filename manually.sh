e6a23597ae7dc6953b05b0911e51f709d7dff2e5
Theo Belaire <theo.belaire@gmail.com>
2015-11-22 09:44:44 -0500

    Remove unused files

--- manually.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-arm-none-eabi-gcc -nostdlib -mthumb-interwork -mthumb -c first.c -o first.gcc.o
-arm-none-eabi-gcc -nostdlib -mthumb-interwork -mthumb -c gba_crt0.s -o gba_crt0.o
-arm-none-eabi-gcc -nostdlib -mthumb-interwork -mthumb -T gba_cart.ld first.gcc.o gba_crt0.o crti.o -o first.gcc.elf
-arm-none-eabi-objcopy -O binary first.gcc.elf first.gcc.gba
-gbafix first.gcc.gba
-open first.gcc.gba
