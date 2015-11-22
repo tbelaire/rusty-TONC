0b0d3d5ce4e830f9c03e0dc00d573d9af3d39565
Theo Belaire <theo.belaire@gmail.com>
2015-11-22 12:10:07 -0500

    Use Cargo
    This puts libcore.rlib in
    .rust/lib/target.json/
    which is automatically searched by cargo, so I don't need to pass a -L
    flag.

--- do-rust.sh
+++ do-rust.sh
@@ -1,15 +1,19 @@
 #!/bin/bash -ex
 
-mkdir -p libs
 mkdir -p out
+LIBS=.rust/lib/target.json/
+mkdir -p $LIBS
 
 # Call rustc to compile libcore for us.
 # This is slow, maybe switch to a makefile?
-[ -e libs/libcore.rlib ] ||
-  rustc --target=target.json -Z no-landing-pads ../rust/src/libcore/lib.rs --out-dir libs
+[ -e $LIBS/libcore.rlib ] ||
+  rustc --target=target.json -Z no-landing-pads ../rust/src/libcore/lib.rs \
+    --out-dir $LIBS
 
-rustc -L libs -Z no-landing-pads --target=target.json src/rust-gba.rs -o out/rust-gba.elf
+#rustc -L libs -Z no-landing-pads --target=target.json src/rust-gba.rs -o out/rust-gba.elf
 
-arm-none-eabi-objcopy -O binary out/rust-gba.elf out/rust-gba.gba
+cargo build --target=target.json --release
+
+arm-none-eabi-objcopy -O binary target/target/release/rust-gba out/rust-gba.gba
 gbafix out/rust-gba.gba
 open out/rust-gba.gba
