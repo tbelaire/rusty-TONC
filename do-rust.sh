4b6987f587f37ac808586090be796c13dafebcde
Theo Belaire <theo.belaire@gmail.com>
2015-11-22 09:57:10 -0500

    Now with specs, we don't need local copies of all these

--- do-rust.sh
+++ do-rust.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -ex
 
-# Compile gba_crt0.s using gcc still.
-arm-none-eabi-gcc -nostdlib -mthumb-interwork -mthumb \
-    -c gba_crt0.s -o gba_crt0.o
-
 mkdir -p libs
 # Call rustc to compile libcore for us.
 # This is slow, maybe switch to a makefile?
