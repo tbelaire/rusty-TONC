1cef43185360f88e068829fce12851f6ad57cc7b
Theo Belaire <theo.belaire@gmail.com>
2015-11-22 10:00:21 -0500

    Moved things into their place

--- do-rust.sh
+++ do-rust.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -ex
 
 mkdir -p libs
+mkdir -p out
+
 # Call rustc to compile libcore for us.
 # This is slow, maybe switch to a makefile?
 [ -e libs/libcore.rlib ] ||
   rustc --target=target.json -Z no-landing-pads ../rust/src/libcore/lib.rs --out-dir libs
 
-rustc -L libs -Z no-landing-pads --target=target.json rust-gba.rs -o rust-gba.elf
+rustc -L libs -Z no-landing-pads --target=target.json src/rust-gba.rs -o out/rust-gba.elf
 
-arm-none-eabi-objcopy -O binary rust-gba.elf rust-gba.gba
-gbafix rust-gba.gba
-open rust-gba.gba
+arm-none-eabi-objcopy -O binary out/rust-gba.elf out/rust-gba.gba
+gbafix out/rust-gba.gba
+open out/rust-gba.gba
